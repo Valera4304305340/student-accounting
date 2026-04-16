772c6628dfc618c0006a24c56c9bc2eb86fb21fa
Valera4304305340 <poltavtzevs@yandex.ru>
2026-04-16 21:56:57 +0600

    Add database migrations and seeders for posts and users tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,13 +16,59 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+      public function run(): void
     {
-        // User::factory(10)->create();
+        // ========== ЗАПОЛНЯЕМ ТАБЛИЦУ posts ==========ы
+        DB::table('posts')->insert([
+            [
+                'title' => 'Первый пост',
+                'slug' => Str::random(10) . '-1',
+                'likes' => rand(10, 100),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'Второй пост',
+                'slug' => Str::random(10) . '-2',
+                'likes' => rand(10, 100),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'Третий пост',
+                'slug' => Str::random(10) . '-3',
+                'likes' => rand(10, 100),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // ========== ЗАПОЛНЯЕМ ТАБЛИЦУ users ==========
+        DB::table('users')->insert([
+            [
+                'name' => 'Иван Петров',
+                'email' => 'ivan@example.com',
+                'age' => 25,
+                'salary' => 50000,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Мария Сидорова',
+                'email' => 'maria@example.com',
+                'age' => 30,
+                'salary' => 75000,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => Str::random(8),
+                'email' => Str::random(6) . '@gmail.com',
+                'age' => rand(18, 60),
+                'salary' => rand(30000, 120000),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
         ]);
     }
 }
